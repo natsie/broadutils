a0e0c0fce4531357c774f2d5a9a495d0ac457d1b
Oghenevwegba Obire <jesseoghenevwegba@outlook.com>
2025-12-15 17:49:50 +0100

    added new type to (/types)

--- dist/types/types.d.ts
+++ dist/types/types.d.ts
@@ -5,6 +5,7 @@ export type Vector3 = [number, number, number];
 export type AnyFunction = (...args: any[]) => any;
 export type CallbackFunction<T extends any[]> = (...args: T) => void;
 export type CallbackFunctionOne<T> = (value: T) => void;
+export type FunctionThatReturns<T, Args extends any[] = []> = (...args: Args) => T;
 export type Enforce<T, U> = T extends U ? T : never;
 export type IfExtendsThenElse<T, Extension, TrueType, FalseType> = T extends Extension ? TrueType : FalseType;
 //# sourceMappingURL=types.d.ts.map
