d9c31aac9f1445434e0b267b596299cc5aacfc2e
Oghenevwegba Obire <jesseoghenevwegba@outlook.com>
2025-12-15 15:48:53 +0100

    added some types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,9 +1,14 @@
+export type Nullish = null | undefined;
+export type OrArray<T> = T | T[];
+
 export type Vector2 = [number, number];
 export type Vector3 = [number, number, number];
 
-export type OrArray<T> = T | T[];
-export type Nullish = null | undefined;
-export type CallbackFunction<T> = (value: T) => void;
+export type AnyFunction = (...args: any[]) => any;
+export type CallbackFunction<T extends any[]> = (...args: T) => void;
+export type CallbackFunctionOne<T> = (value: T) => void;
+
+export type Enforce<T, U> = T extends U ? T : never;
 export type IfExtendsThenElse<T, Extension, TrueType, FalseType> = T extends Extension
   ? TrueType
   : FalseType;
